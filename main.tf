be730b71d0782cdf845a6ea348190d52eb51ac96
Rodrigo Valdés <rodrigovaldes@gmail.com>
2022-01-08 01:05:38 -0300

    Configuración inicial.

---
+++ main.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = file("devops-2020-337523-46f61b034ef2.json")
+
+  project = "devops-2020-337523"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "devops-2022-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "devops-instance"
+  machine_type = "e2-medium"
+  tags         = ["docker", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "projects/centos-cloud/global/images/centos-8-v20211214"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "devops-2022-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22", "80", "8080", "1000-2000"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
