0c75660f3e2e9c0e9c564667ba8d06336fb64669
Rodrigo Valdés <rodrigovaldes@gmail.com>
2022-01-08 03:44:14 -0300

    Bootstrap de git docker y robot user

--- main.tf
+++ main.tf
@@ -25,11 +25,17 @@ resource "google_compute_instance" "vm_instance" {
   tags         = ["docker", "dev"]
   metadata = {
     startup-script = <<-EOF
-    echo "hello you" > /test.txt
-    echo "helpme" > /test2.txt
     echo "Bootstrapping software layer....."
     yum update
-    yum install -y git
+    yum install -y git yum-utils
+    yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    yum install -y docker-ce docker-ce-cli containerd.io
+    systemctl enable docker
+    systemctl start docker
+    groupadd docker
+    adduser robot
+    usermod -aG wheel robot
+    usermod -aG docker robot
   EOF
   } 
 
