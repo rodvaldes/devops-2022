8fd83ac5d09f96cd43a86178b581c753604828b8
Rodrigo Valdés <rodrigovaldes@gmail.com>
2022-01-09 04:02:49 -0300

    Update de instalación Bamboo y fix de path en properties

--- init.sh
+++ init.sh
@@ -11,6 +11,7 @@ adduser robot
 usermod -aG wheel robot
 usermod -aG docker robot
 firewall-cmd --zone=public --add-service=http
+firewall-cmd --zone=public --permanent --add-port 8085/tcp
 firewall-cmd --reload
 yum install -y httpd
 systemctl enable httpd
@@ -24,11 +25,20 @@ git clone https://github.com/rodvaldes/devops-2022.git /home/robot/devops-2022
 chown -R robot: /home/robot/devops-2022
 dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
 dnf -qy module disable postgresql
-dnf install -y postgresql14-server
+dnf install -y postgresql14-server wget
 /usr/pgsql-14/bin/postgresql-14-setup initdb
 systemctl enable postgresql-14
 systemctl start postgresql-14
 sudo -u postgres bash -c "psql -c \"CREATE USER bamboouser WITH PASSWORD 'bamboouserpass';\""
 sudo -u postgres bash -c "psql -c \"CREATE DATABASE bamboo_db WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;\""
 sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE bamboo_db to bamboouser;\""
-
+wget https://storage.googleapis.com/devops-2022/atlassian-bamboo-8.1.1.tar.gz
+adduser bamboo
+usermod -aG wheel bamboo
+mkdir -p /opt/atlassian/bamboo
+tar -xvzf atlassian-bamboo-8.1.1.tar.gz -C /opt/atlassian/bamboo
+chown -R bamboo: /opt/atlassian/bamboo
+mkdir -p /var/atlassian/bamboo/atlassian-bamboo-8.1.1
+cp -f /home/robot/devops-2022/config/bamboo/bamboo-init.properties /opt/atlassian/bamboo/atlassian-bamboo-8.1.1/atlassian-bamboo/WEB-INF/classes
+chown -R bamboo: /var/atlassian/bamboo
+sudo -u bamboo bash -c "sh /var/atlassian/bamboo/atlassian-bamboo-8.1.1/bin/start-bamboo.sh"
