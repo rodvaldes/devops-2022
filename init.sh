9181008a7d93c6a3d1563a5fbb8b9ddfb8097882
Rodrigo Valdés <rodrigovaldes@gmail.com>
2022-01-09 02:27:11 -0300

    Automatización  instalacion y config de postgres para Bamboo.

--- init.sh
+++ init.sh
@@ -22,3 +22,13 @@ yum install -y java-1.8.0-openjdk
 yum install -y maven
 git clone https://github.com/rodvaldes/devops-2022.git /home/robot/devops-2022
 chown -R robot: /home/robot/devops-2022
+dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-8-x86_64/pgdg-redhat-repo-latest.noarch.rpm
+dnf -qy module disable postgresql
+dnf install -y postgresql14-server
+/usr/pgsql-14/bin/postgresql-14-setup initdb
+systemctl enable postgresql-14
+systemctl start postgresql-14
+sudo -u postgres bash -c "psql -c \"CREATE USER bamboouser WITH PASSWORD 'bamboouserpass';\""
+sudo -u postgres bash -c "psql -c \"CREATE DATABASE bamboo_db WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' TEMPLATE template0;\""
+sudo -u postgres bash -c "psql -c \"GRANT ALL PRIVILEGES ON DATABASE bamboo_db to bamboouser;\""
+
